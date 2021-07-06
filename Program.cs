419b3197e235e2ab92bafbadb0b0cc2dd78c9498
kenneth1175 <53136158+kenneth1175@users.noreply.github.com>
2021-07-06 16:27:50 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTribalscale_Automation
+{
+    class MainClass
+    {
+        
+        public static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            //Create a refrerence for Chrome Browser
+            IWebDriver driver = new ChromeDriver();
+
+        // Go to Tribal App
+        driver.Navigate().GoToUrl("https://qainterview.pythonanywhere.com");
+    
+
+        // Make the browser full screen
+            driver.Manage().Window.Maximize();
+
+        //Find the element
+            IWebElement searchBox = driver.FindElement(By.Name("number"));
+
+         // Type something in the search bar
+            searchBox.SendKeys("7");
+        
+
+         //Close the browser
+            driver.Quit();
+        }
+
+
+    }
+}
